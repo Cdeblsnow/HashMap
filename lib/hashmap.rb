0c5a6f4b1870a5d4229179fdd1e3d06c36e574b1
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-01-03 20:19:34 -0500

    Add methods get and has?

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,9 +1,15 @@
 require "rubocop"
+require_relative "node"
 
 class HashMap
+  attr_accessor :bucket
+
   def initialize
     @load_factor = 0.8
     @capacity = 16
+    @bucket = Array.new(@capacity, "")
+    @head = nil
+    @tail = nil
   end
 
   def hash(key)
@@ -16,5 +22,53 @@ class HashMap
   end
 
   def set(key, value)
+    index = hash(key) % 16
+    if @bucket[index] == ""
+      @bucket[index] = append([key, value]) # insert when empty
+    elsif @bucket[index].value[0] == key
+      @bucket[index].value[1] = value # update
+    else
+      @bucket[index].append([key, value]) # insert
+
+    end
+  end
+
+  def append(value)
+    @node = Node.new # remove
+    @node.value = value
+    if @head.nil?
+      @head = @node
+      @tail = @node
+    else
+      @node.next_node = @head
+      @head = @node
+    end
+  end
+
+  def get(key)
+    index = hash(key) % 16
+    @bucket[index].each do |node|
+      return nil if node.nil? # check
+
+      puts node.value[1] if node.value[0] == key
+    end
+  end
+
+  def has?(key)
+    index = hash(key) % 16
+    @bucket[index].each do |node|
+      return False if node.nil? # check
+
+      return True if node.value[0] == key
+    end
+  end
+
+  def remove(key)
+    index = hash(key) % 16
+    @bucket[index].each do |node|
+      return False if node.nil?
+
+      return True if node.value[0] == key
+    end
   end
 end
