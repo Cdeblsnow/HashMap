170b78291b21ab5b5d14bc291ef4590fa87ac28b
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-01-08 21:15:06 -0500

    Add append method and change initialize

--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,22 @@
 class Node
-  attr_accessor :value, :next_node
+  attr_accessor :value, :next_node, :head, :tail
 
-  def initialize
-    @value = nil
+  def initialize(value)
+    @value = value
     @next_node = nil
+    @head = self
+    @tail = self
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+
+    end
   end
 end
